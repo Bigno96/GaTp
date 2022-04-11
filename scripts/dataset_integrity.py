2dffd16fae6d0629d4f8f0adbc4fd61d6663c1ee
Bigno96 <Andrea.bignoli96@gmail.com>
2022-04-11 16:48:39 +0200

    Checked loss prediction

--- scripts/dataset_integrity.py
+++ scripts/dataset_integrity.py
@@ -5,19 +5,18 @@ from easydict import EasyDict
 from tqdm import tqdm
 
 import data_loading.data_loader as loader
-from models.basic.mlp import MLP
 
 
 def check_integrity():
 
     # put manually values into config Namespace
     config = EasyDict()
-    config.batch_size = 10
+    config.batch_size = 5
     config.valid_batch_size = 1
     config.test_batch_size = 1
     config.data_loader_workers = 0
     config.pin_memory = True
-    config.data_root = 'D:/Uni/TESI/GaTp/datasets'
+    config.data_root = '/home/bigno/Bigno/Uni/TESI/GaTp/datasets'
     config.map_type = 'random_grid'
     config.map_shape = [20, 20]
     config.map_density = 0.1
@@ -44,11 +43,6 @@ def check_integrity():
     print(f'Checking Train Dataset')
     time.sleep(.1)  # printing sync
 
-    # toy model
-    model = MLP(in_features=int(3 * 11 * 11),
-                out_features=5,
-                hidden_features=(5000, 500))
-
     '''training dataset test'''
     # batch input shape = batch_size, num_agent, 3, fov_h, fov_w
     # batch gso shape = batch_size, num_agent, num_agent
@@ -56,20 +50,9 @@ def check_integrity():
     for batch_input, batch_GSO, batch_target, basename \
             in tqdm(train_dl.train_loader):
 
-        predict = model(batch_input.reshape(config.batch_size * config.agent_number, 3 * 11 * 11))
-        print(f'predict: {predict}')
-
-        batch_target = batch_target.reshape(config.batch_size * config.agent_number, 5)
-        print(f'max batch target: {torch.max(batch_target, 1)[1]}')
-
-        loss = torch.nn.CrossEntropyLoss(predict, torch.max(batch_target, 1)[1])
-        print(f'loss {loss}')
-
-        # check_corruption(batch_input, basename)
-        # check_corruption(batch_GSO, basename)
-        # check_corruption(batch_target, basename)
-
-        input('Oscar è scemoh\n')
+        check_corruption(batch_input, basename)
+        check_corruption(batch_GSO, basename)
+        check_corruption(batch_target, basename)
 
     time.sleep(.1)  # printing sync
     print(f'Checking Valid Dataset')
@@ -108,6 +91,7 @@ def check_integrity():
 def check_corruption(t, basename):
     if not torch.all(torch.isfinite(t)).tolist():
         print(f'Corrupted input in {basename}')
+        print(t)
 
 
 if __name__ == '__main__':
